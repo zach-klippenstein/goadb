f8a8785326cf872e3b59e0249355583f62e59f89
Zach Klippenstein <zach.klippenstein@gmail.com>
2016-01-08 10:23:20 -0800

    Added command output to StartServer error.

--- server_controller.go
+++ server_controller.go
@@ -2,6 +2,7 @@ package goadb
 
 import (
 	"os/exec"
+	"strings"
 
 	"github.com/zach-klippenstein/goadb/util"
 )
@@ -11,6 +12,7 @@ StartServer ensures there is a server running.
 */
 func StartServer() error {
 	cmd := exec.Command("adb", "start-server")
-	err := cmd.Run()
-	return util.WrapErrorf(err, util.ServerNotAvailable, "error starting server: %s", err)
+	output, err := cmd.CombinedOutput()
+	outputStr := strings.TrimSpace(string(output))
+	return util.WrapErrorf(err, util.ServerNotAvailable, "error starting server: %s\noutput:\n%s", err, outputStr)
 }
