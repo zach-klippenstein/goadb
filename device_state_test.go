49dbce382b31b0bd14834393a15a21acb8e56102
Zach Klippenstein <zach.klippenstein@gmail.com>
2016-05-21 23:23:26 -0700

    Make Device.State return DeviceState instead of string.

---
+++ device_state_test.go
@@ -0,0 +1,31 @@
+package adb
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseDeviceState(t *testing.T) {
+	for _, test := range []struct {
+		String    string
+		WantState DeviceState
+		WantName  string
+		WantError error // Compared by Error() message.
+	}{
+		{"", StateDisconnected, "StateDisconnected", nil},
+		{"offline", StateOffline, "StateOffline", nil},
+		{"device", StateOnline, "StateOnline", nil},
+		{"bad", StateInvalid, "StateInvalid", errors.New(`ParseError: invalid device state: "StateInvalid"`)},
+	} {
+		state, err := parseDeviceState(test.String)
+		if test.WantError == nil {
+			assert.NoError(t, err)
+		} else {
+			assert.EqualError(t, err, test.WantError.Error())
+		}
+		assert.Equal(t, test.WantState, state)
+		assert.Equal(t, test.WantName, state.String())
+	}
+}
